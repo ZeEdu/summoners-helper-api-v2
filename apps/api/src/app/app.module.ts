ab2577e3f0e1dfc57b80f6ffc6a2cc8e09b41f79
eduardo <eduardo.carlos.siq@gmail.com>
2026-06-03 18:11:04 -0300

    adiciona MongooseModule.forFeature

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { JwtStrategy } from './auth/strategy/jwt.strategy';
 import { LoggerMiddleware } from '../middlewares/logger.middleware';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { User, UserSchema } from './users/schema/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/summoners-helper'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
--- apps/api/src/app/users/users.module.ts
+++ apps/api/src/app/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controller/users.controller';
 import { UsersService } from './service/users.service';
-
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
