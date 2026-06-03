7729e8553881af12deeb266a7a3bbc1291b6598d
eduardo <eduardo.carlos.siq@gmail.com>
2026-06-03 18:18:09 -0300

    corrige problemas de dependencia

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -5,9 +5,11 @@ import { JwtStrategy } from './strategy/jwt.strategy';
 import { LocalStrategy } from './strategy/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
