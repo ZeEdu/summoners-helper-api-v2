bd2992123cadba9bd489dfca51de15018984420b
eduardo <eduardo.carlos.siq@gmail.com>
2026-08-19 11:40:31 -0300

    build-creation: cria um componente de Stepper para ser utilizado no formulário

---
+++ apps/mobile/src/components/stepper/StepperFooter.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import { View } from "react-native";
+import { Button } from "react-native-paper";
+
+import { useStepperContext } from "./Stepper";
+
+type Props = {
+  customPreviousButtonText?: string;
+  customNextButtonText?: string;
+  customNextButton?: () => ReactNode
+}
+
+export default function StepperFooter({
+  customPreviousButtonText,
+  customNextButtonText,
+  customNextButton
+}: Props) {
+  const stepperContext = useStepperContext()
+
+  return (
+    <View style={{ flexDirection: 'row', marginBottom: 16, gap: 8 }}>
+      <Button
+        mode="contained-tonal"
+        style={{ flex: 1 }}
+        disabled={stepperContext.disablePreviousButton}
+        onPress={() => {
+          stepperContext.previousStep()
+        }}>
+        {customPreviousButtonText || 'Passo anterior'}
+      </Button>
+      {customNextButton ? (
+        customNextButton()
+      ) : (
+        <Button
+          mode="contained"
+          style={{ flex: 1 }}
+          disabled={stepperContext.disableNextButton}
+          onPress={() => {
+            stepperContext.nextStep()
+          }}
+        >
+          {customNextButtonText || 'Próximo passo'}
+        </Button>
+      )
+      }
+    </View>
+  )
+}
